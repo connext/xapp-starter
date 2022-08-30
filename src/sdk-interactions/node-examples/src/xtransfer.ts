e26730a49bab5c6539d009d95a65b18b45d61ab8
just-a-node <eye1717@gmail.com>
2022-08-29 18:30:52 -0600

    sdk updates

--- src/sdk-interactions/node-examples/src/xtransfer.ts
+++ src/sdk-interactions/node-examples/src/xtransfer.ts
@@ -17,21 +17,21 @@ const callParams = {
   callData: "0x", // empty calldata for a simple transfer
   originDomain: "1735353714", // send from Goerli
   destinationDomain: "1735356532", // to Optimism-Goerli
-  agent: signerAddress, // address allowed to transaction on destination side in addition to relayers
-  recovery: signerAddress,
-  forceSlow: false, // option to force Nomad slow path (~30 mins) instead of paying 0.05% fee
-  receiveLocal: false, // option to receive the local Nomad-flavored asset instead of the adopted asset
-  callback: ethers.constants.AddressZero, // no callback so use the zero address
-  callbackFee: "0", // fee paid to relayers for the callback; no fees on testnet
-  relayerFee: "0", // fee paid to relayers for the forward call; no fees on testnet
-  destinationMinOut: (amount * 0.99).toString(), // accept a 1% slippage tolerance on the destination-side stableswap
+  agent: signerAddress, // address allowed to execute transaction on destination side in addition to relayers
+  recovery: signerAddress, // fallback address to send funds to if execution fails on destination side
+  forceSlow: false, // option to force slow path instead of paying 0.05% fee on fast liquidity transfers
+  receiveLocal: false, // option to receive the local bridge-flavored asset instead of the adopted asset
+  callback: ethers.constants.AddressZero, // zero address because we don't expect a callback
+  callbackFee: "0", // fee paid to relayers; relayers don't take any fees on testnet
+  relayerFee: "0", // fee paid to relayers; relayers don't take any fees on testnet
+  destinationMinOut: (amount * 0.97).toString(), // the minimum amount that the user will accept due to slippage from the StableSwap pool (3% here)
 };
 
 const xCallArgs = {
   params: callParams,
   transactingAsset: "0x7ea6eA49B0b0Ae9c5db7907d139D9Cd3439862a1", // the Goerli Test Token
   transactingAmount: amount.toString(), 
-  originMinOut: (amount * 0.99).toString() // accept a 1% slippage tolerance on the origin-side stableswap
+  originMinOut: (amount * 0.97).toString() // the minimum amount that the user will accept due to slippage from the StableSwap pool (3% here)
 };
 
 // Approve the asset transfer because we're sending funds
