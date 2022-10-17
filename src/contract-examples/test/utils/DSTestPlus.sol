075a813507e8a7a879f8a116899ff61a45b776b4
just-a-node <eye1717@gmail.com>
2022-10-17 16:55:07 -0600

    Add extra testnet domains

--- src/contract-examples/test/utils/DSTestPlus.sol
+++ src/contract-examples/test/utils/DSTestPlus.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.8.10;
 
 import {XConsole} from "./Console.sol";
 
-import {ERC20} from "@solmate/tokens/ERC20.sol";
-
 import {Vm} from "@std/Vm.sol";
 import {Test} from "forge-std/Test.sol";
 
@@ -14,8 +12,12 @@ contract DSTestPlus is Test {
   // Domain IDs for testnet
   uint32 public goerliDomainId = 1735353714;
   uint32 public optimismGoerliDomainId = 1735356532;
+  uint32 public polygonMumbaiDomainId = 9991;
+  uint32 public arbitrumGoerliDomainId = 1734439522;
 
   // Chain IDs
   uint32 public goerliChainId = 5;
   uint32 public optimismGoerliChainId = 420;
+  uint32 public mumbaiChainId = 80001;
+  uint32 public arbitrumGoerliChainId = 421613;
 }
