aa94e709747fad3e0ac3d35c1b248d4697a6d9d6
just-a-node <eye1717@gmail.com>
2022-10-17 18:47:45 -0600

    Fix TransferToken tests

--- src/contract-examples/test/transfer-token/TransferToken.t.sol
+++ src/contract-examples/test/transfer-token/TransferToken.t.sol
@@ -2,133 +2,155 @@
 pragma solidity ^0.8.15;
 
 import {TransferToken} from "../../transfer-token/TransferToken.sol";
-import {IConnext} from "@nxtp/core/connext/interfaces/IConnext.sol";
+import {IConnext} from "@connext/nxtp-contracts/contracts/core/connext/interfaces/IConnext.sol";
 import {DSTestPlus} from "../utils/DSTestPlus.sol";
-import {MockERC20} from "@solmate/test/utils/mocks/MockERC20.sol";
+import {ERC20PresetMinterPauser} from "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
  * @title TransferTokenTestUnit
  * @notice Unit tests for TransferToken.
  */
 contract TransferTokenTestUnit is DSTestPlus {
-  MockERC20 private token;
-  address private connext;
-  TransferToken private transfer;
+  ERC20PresetMinterPauser public token;
+  IConnext public connext = IConnext(address(1));
+  TransferToken public transfer;
+  address public userChainA = address(0xA);
+  address public userChainB = address(0xB);
 
   function setUp() public {
-    connext = address(1);
-    token = new MockERC20("TestToken", "TEST", 18);
-    transfer = new Transfer(IConnextHandler(connext));
+    token = new ERC20PresetMinterPauser("TestToken", "TEST");
+    transfer = new TransferToken(connext);
 
-    vm.label(connext, "ConnextHandler");
-    vm.label(address(transfer), "Transfer");
+    vm.label(address(connext), "Connext");
+    vm.label(address(transfer), "TransferToken");
     vm.label(address(token), "TestToken");
     vm.label(address(this), "TestContract");
+    vm.label(userChainA, "userChainA");
+    vm.label(userChainB, "userChainB");
   }
 
-  function testTransferEmitsTransferInitiated() public {
-    address userChainA = address(0xA);
-    address userChainB = address(0xB);
-    vm.label(address(userChainA), "userChainA");
-    vm.label(address(userChainB), "userChainB");
-
-    // TODO: fuzz this
-    uint256 amount = 10_000;
-
-    // Grant the user some tokens
-    token.mint(address(userChainA), amount);
+  function test_TransferToken_transferShouldTransferTokensFromCaller(uint256 amount) public {
+    // Mint userChainA some tokens
+    token.mint(userChainA, amount);
     console.log(
       "userChainA TestToken balance",
-      token.balanceOf(address(userChainA))
+      token.balanceOf(userChainA)
     );
 
-    // User must approve transfer to transfer contract
+    // userChainA must approve transfer to TransferToken contract
     vm.prank(userChainA);
     token.approve(address(transfer), amount);
 
     // Mock the xcall
-    bytes memory mockxcall = abi.encodeWithSelector(
-      IConnextHandler.xcall.selector
+    bytes memory xcall = abi.encodeWithSelector(
+      IConnext.xcall.selector
     );
-    vm.mockCall(connext, mockxcall, abi.encode(1));
+    vm.mockCall(address(connext), xcall, abi.encode(1));
 
-    // Check for an event emitted
-    vm.expectEmit(true, true, true, true);
-    emit TransferInitiated(
-      address(token),
-      address(userChainA),
-      address(userChainB)
+    // Test that tokens are sent from userChainA to TransferToken contract
+    vm.expectCall(
+      address(token), 
+      abi.encodeCall(
+        IERC20.transferFrom, 
+        (
+          userChainA, 
+          address(transfer),
+          amount
+        )
+      )
     );
 
-    vm.prank(address(userChainA));
+    vm.prank(userChainA);
     transfer.transfer(
-      address(userChainB),
-      address(token),
-      goerliDomainId,
+      userChainB,
       optimismGoerliDomainId,
-      amount
+      address(token),
+      amount,
+      0
     );
   }
 }
 
 /**
- * @title TransferTestForked
- * @notice Integration tests for Transfer. Should be run with forked testnet (Goerli).
+ * @title TransferTokenTestForked
+ * @notice Integration tests for TransferToken. Should be run with forked testnet.
  */
-contract TransferTestForked is DSTestPlus {
-  // Testnet Addresses
-  address public connext = 0xD9e8b18Db316d7736A3d0386C59CA3332810df3B;
-  address public constant testToken = 0x7ea6eA49B0b0Ae9c5db7907d139D9Cd3439862a1;
+contract TransferTokenTestForked is DSTestPlus {
+  // Staging testnet addresses on Goerli
+  IConnext public connext = IConnext(0x9590e2bB6a93e2a531b0269eE7396cECc3E5d6eA);
+  IERC20 public token = IERC20(0x7ea6eA49B0b0Ae9c5db7907d139D9Cd3439862a1);
+  address public userChainA = address(0xA);
+  address public userChainB = address(0xB);
+  address public whaleChainA = address(0x6d2A06543D23Cc6523AE5046adD8bb60817E0a94); 
 
-  Transfer private transfer;
-  MockERC20 private token;
-
-  event TransferInitiated(address asset, address from, address to);
+  TransferToken private transfer;
 
   function setUp() public {
-    transfer = new Transfer(IConnextHandler(connext));
-    token = MockERC20(testToken);
+    transfer = new TransferToken(connext);
 
-    vm.label(connext, "Connext");
-    vm.label(address(transfer), "Transfer");
+    vm.label(address(connext), "Connext");
+    vm.label(address(transfer), "TransferToken");
     vm.label(address(token), "TestToken");
     vm.label(address(this), "TestContract");
+    vm.label(userChainA, "userChainA");
+    vm.label(userChainB, "userChainB");
+    vm.label(whaleChainA, "whaleChainA");
   }
 
-  function testTransferEmitsTransferInitiated() public {
-    address userChainA = address(0xA);
-    address userChainB = address(0xB);
-    vm.label(address(userChainA), "userChainA");
-    vm.label(address(userChainB), "userChainB");
+  function test_TransferToken_transferShouldCallXCall(uint256 amount) public {
+    // Whale should have enough funds for this test case
+    vm.assume(token.balanceOf(whaleChainA) >= amount);
 
-    // TODO: fuzz this
-    uint256 amount = 10_000;
-
-    // Grant the user some tokens
-    token.mint(address(userChainA), amount);
+    // Send userChainA some tokens
+    vm.prank(whaleChainA);
+    token.transfer(userChainA, amount);
     console.log(
       "userChainA TestToken balance",
-      token.balanceOf(address(userChainA))
+      token.balanceOf(userChainA)
     );
 
-    // User must approve transfer to transfer
+    // userChainA must approve transfer to TransferToken contract
     vm.prank(userChainA);
     token.approve(address(transfer), amount);
 
-    vm.expectEmit(true, true, true, true);
-    emit TransferInitiated(
-      address(token),
-      address(userChainA),
-      address(userChainB)
+    // Test that tokens are sent from userChainA to TransferToken contract
+    vm.expectCall(
+      address(token), 
+      abi.encodeCall(
+        IERC20.transferFrom, 
+        (
+          userChainA, 
+          address(transfer),
+          amount
+        )
+      )
     );
 
-    vm.prank(address(userChainA));
+    // Test that xcall is called
+    vm.expectCall(
+      address(connext), 
+      abi.encodeCall(
+        IConnext.xcall, 
+        (
+          optimismGoerliDomainId,
+          userChainB,
+          address(token),
+          userChainA,
+          amount,
+          3,
+          ""
+        )
+      )
+    );
+
+    vm.prank(userChainA);
     transfer.transfer(
-      address(userChainB),
-      address(token),
-      goerliDomainId,
+      userChainB,
       optimismGoerliDomainId,
-      amount
+      address(token),
+      amount,
+      0
     );
   }
 }
