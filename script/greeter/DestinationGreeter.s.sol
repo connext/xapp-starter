917f3385f0f7faa0791010ac29dfe2e12b870bff
just-a-node <eye1717@gmail.com>
2023-02-08 23:51:49 -0700

    Update imports

--- script/greeter/DestinationGreeter.s.sol
+++ script/greeter/DestinationGreeter.s.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.15;
 
-import "forge-std/Script.sol";
+import "@std/Script.sol";
 import {DestinationGreeter} from "../../src/contract-examples/greeter/DestinationGreeter.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DeployDestinationGreeter is Script {
   function run(address _token) external {
     vm.startBroadcast();
 
-    new DestinationGreeter(_token);
+    new DestinationGreeter(IERC20(_token));
 
     vm.stopBroadcast();
   }
--- script/greeter/SourceGreeter.s.sol
+++ script/greeter/SourceGreeter.s.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.15;
 
-import "forge-std/Script.sol";
-import {IConnext} from "@connext/nxtp-contracts/contracts/core/connext/interfaces/IConnext.sol";
+import "@std/Script.sol";
 import {SourceGreeter} from "../../src/contract-examples/greeter/SourceGreeter.sol";
+import {IConnext} from "@connext/nxtp-contracts/contracts/core/connext/interfaces/IConnext.sol";
 
 contract DeploySourceGreeter is Script {
   function run(address _connext) external {
